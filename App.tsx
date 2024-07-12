dc53a881f38a6683990e9b9a7647f3eaa044f24d
nccuongdev <nccuong.dev.mobile@gmail.com>
2024-07-12 16:06:10 +0700

    useState2

--- App.tsx
+++ App.tsx
@@ -2,9 +2,10 @@ import {View, Text, SafeAreaView, ScrollView} from 'react-native';
 import React from 'react';
 import MainScreen from './src/BasicAndComponent/LectureTwo/screen/MainScreen';
 import MainScreen2 from './src/StateAndProps/LectureOne/screen/MainScreen2';
+import MainScreen3 from './src/StateAndProps/LectureTwo/screen/MainScreen3';
 
 const App = () => {
-  return <MainScreen2 />;
+  return <MainScreen3 />;
 };
 
 export default App;
---
+++ src/StateAndProps/LectureTwo/screen/MainScreen3.tsx
@@ -0,0 +1,53 @@
+import {
+  SafeAreaView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+import React, {useState} from 'react';
+
+export default function MainScreen3() {
+  const [backgroundColor, setBackgroundColor] = useState('#fff');
+  const generateBackground = () => {
+    let color = '#';
+    const hexRange = '0123456789ABCDEF';
+
+    for (let i = 0; i < 6; i++) {
+      color += hexRange[Math.floor(Math.random() * 16)];
+    }
+    console.log(color);
+
+    setBackgroundColor(color);
+  };
+
+  return (
+    <>
+      <StatusBar backgroundColor={backgroundColor} />
+      <SafeAreaView
+        style={[styles.contaier, {backgroundColor: backgroundColor}]}>
+        <View>
+          <TouchableOpacity
+            onPress={generateBackground}
+            style={styles.buttonStyle}>
+            <Text>Random color</Text>
+          </TouchableOpacity>
+        </View>
+      </SafeAreaView>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  buttonStyle: {
+    backgroundColor: '#fff',
+    padding: 10,
+    borderRadius: 4,
+  },
+  contaier: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    flex: 1,
+  },
+});
